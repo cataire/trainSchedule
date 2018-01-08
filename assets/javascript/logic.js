48a6ae5a7619b5da852bd15679e03d3eed7b729f
cataire <mail.for.mu@gmail.com>
2018-01-07 20:49:49 -0700

    initial

---
+++ assets/javascript/logic.js
@@ -0,0 +1,75 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC57wb14iCyvJH0-Vv1dVbu_rfLI8PpAS4",
+    authDomain: "my-cool-project-fe0c8.firebaseapp.com",
+    databaseURL: "https://my-cool-project-fe0c8.firebaseio.com",
+    projectId: "my-cool-project-fe0c8",
+    storageBucket: "my-cool-project-fe0c8.appspot.com",
+    messagingSenderId: "847247850535"
+  };
+  firebase.initializeApp(config);
+
+const database = firebase.database();
+var startOfDay = moment().startOf('day').format("HH:MM");
+console.log(startOfDay);
+
+
+// on button click
+$(".addTrainBtn").on("click", function(event) {
+  event.preventDefault();
+
+  // Grabs fields input
+  var trainNumber = $(".train-name").val().trim();
+  var trainDestination = $(".destination").val().trim();
+  var trainFirstDeparture = $(".firstDeparture").val().trim();
+  var trainFrequency = $(".frequency").val().trim();
+  
+
+  // local object to hold data
+
+  var trainObject = {
+  trainNumber: trainNumber,
+  trainDestination: trainDestination,
+  trainFrequency: trainFrequency,
+  trainFirstDeparture: trainFirstDeparture
+
+  };
+
+  // uploads train info to database
+
+  database.ref().push(trainObject);
+
+  console.log(trainObject.trainNumber);
+  console.log(trainObject.trainDestination);
+  console.log(trainObject.trainFrequency);
+  console.log(trainObject.trainFirstDeparture);
+
+  alert("Train successfully added");
+
+  // Clears all of the text-boxes
+  $(".train-name").val("");
+  $(".destination").val("");
+  $(".firstDeparture").val("");
+  $(".frequency").val("");
+
+});
+
+database.ref().on("child_added", function (childSnapshot, prevChildKey){
+
+	var trainNumber = childSnapshot.val().trainNumber;
+  var trainDestination = childSnapshot.val().trainDestination;
+  var trainFrequency = childSnapshot.val().trainFrequency;
+  var trainFirstDeparture = childSnapshot.val().trainFirstDeparture;
+
+  var timeAfterFirstDeparture = moment(trainFirstDeparture, "HH:MM").diff(moment().format("HH:MM"), "minutes");
+  console.log(timeAfterFirstDeparture);
+  var remainder = timeAfterFirstDeparture%trainFrequency;
+  var minutesAway = trainFrequency - remainder;
+  console.log(minutesAway);
+
+
+  $(".train-schedule > tbody").append("<tr><td>" + trainNumber + "</td><td>" + trainDestination + "</td><td>" +
+  trainFrequency + "</td><td>" + trainFirstDeparture + "</td>");
+
+});
